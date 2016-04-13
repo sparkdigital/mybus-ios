78f9447676fdbed002cfa067d589f6d8a6d5f8b2
Marcos Vivar <mvivar@devspark.com>
2016-04-13 14:45:36 -0300

    Created Connectivity service class and implemented the municipality callejero_mgp endpoint call.

---
+++ MyBus/MyBus/Services/Connectivity.swift
@@ -0,0 +1,95 @@
+//
+//  Connectivity.swift
+//  MyBus
+//
+//  Created by Marcos Vivar on 4/13/16.
+//  Copyright © 2016 Spark Digital. All rights reserved.
+//
+
+import Alamofire
+import SwiftyJSON
+
+private let _sharedInstance = Connectivity()
+
+private let municipalityAccessToken = "rwef3253465htrt546dcasadg4343"
+
+private let municipalityBaseURL = "http://gis.mardelplata.gob.ar/opendata/ws.php?method=rest"
+
+private let streetNamesEndpointURL = "\(municipalityBaseURL)&endpoint=callejero_mgp&token=\(municipalityAccessToken)&nombre_calle="
+
+private let addressToCoordinateEndpointURL = "\(municipalityBaseURL)&endpoint=callealtura_coordenada&token=\(municipalityAccessToken)"
+
+private let coordinateToAddressEndpointURL = "\(municipalityBaseURL)&endpoint=coordenada_calleaaltura&token=\(municipalityAccessToken)"
+
+private let login_state = "login_state"
+
+public class Connectivity: NSObject
+{
+    // MARK: - Instantiation
+    public class var sharedInstance: Connectivity
+    {
+        return _sharedInstance
+    }
+    
+    private var loginState : Bool
+        {
+        get
+        {
+            let returnLoginState: Bool = NSUserDefaults.standardUserDefaults().boolForKey(login_state)
+            
+            return returnLoginState
+        }
+        set (newValue)
+        {
+            NSUserDefaults.standardUserDefaults().setBool(newValue, forKey:login_state)
+        }
+    }
+    
+    override init() { }
+    
+    public func getLoginStatus() -> Bool
+    {
+        return self.loginState
+    }
+    
+    private func setLoginStatus(value: Bool)
+    {
+        self.loginState = value
+    }
+    
+    // MARK: Municipality Endpoints
+    public func getStreetNames(forName streetName: String)
+    {
+        let streetNameURLString = "\(streetNamesEndpointURL)\(streetName)"
+        
+        let request = NSMutableURLRequest(URL: NSURL(string: streetNameURLString)!)
+        request.HTTPMethod = "GET"
+        
+        Alamofire.request(request).responseJSON { (response) -> Void in
+            
+            if(response.result.isFailure)
+            {
+                print("\nError: \(response.result.error!)")
+            }
+            else
+            {
+                let resultValue = response.result.value!
+                
+                print(resultValue)
+                
+                var bodyData: NSData?
+                do
+                {
+                    bodyData = try NSJSONSerialization.dataWithJSONObject(resultValue, options: .PrettyPrinted)
+                    print(bodyData)
+                }
+                catch
+                {
+                    bodyData = nil
+                    print(error)
+                }
+            }
+        }
+    }
+    
+}
