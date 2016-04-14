7493c89629cfe26e17f37f8fed0a5be2c0f17726
Lisandro Falconi <lisandrofalconi@gmail.com>
2016-04-13 22:32:10 -0300

    Handle single tap on map, add a marker and fetch address from geolocation

--- MyBus/MyBus/ViewController.swift
+++ MyBus/MyBus/ViewController.swift
@@ -20,11 +20,40 @@ class ViewController: UIViewController, MGLMapViewDelegate {
         
         mapView.userTrackingMode = .Follow
         mapView.delegate = self
+        
+        // Double tapping zooms the map, so ensure that can still happen
+        let doubleTap = UITapGestureRecognizer(target: self, action: nil)
+        doubleTap.numberOfTapsRequired = 2
+        mapView.addGestureRecognizer(doubleTap)
+        
+        // Delay single tap recognition until it is clearly not a double
+        let singleTap = UITapGestureRecognizer(target: self, action: #selector(ViewController.handleSingleTap(_:)))
+        singleTap.requireGestureRecognizerToFail(doubleTap)
+        mapView.addGestureRecognizer(singleTap)
     }
     
     func mapView(mapView: MGLMapView, didUpdateUserLocation userLocation: MGLUserLocation?) {
         //Move map to current user location
         mapView.centerCoordinate = (userLocation!.location?.coordinate)!
+    }
+    
+    func handleSingleTap(tap: UITapGestureRecognizer) {
+        // Convert tap location (CGPoint) to geographic coordinates (CLLocationCoordinate2D)
+        let tappedLocation: CLLocationCoordinate2D = mapView.convertPoint(tap.locationInView(mapView), toCoordinateFromView: mapView)
+        
+        // Declare the marker point and set its coordinates
+        let mapPoint = MGLPointAnnotation()
+        mapPoint.coordinate = CLLocationCoordinate2D(latitude: tappedLocation.latitude, longitude: tappedLocation.longitude)
+        
+        // Remove first marker tapped from the map, add marker with coordinates
+        // Prevent having more than two points selected in map
+        if (mapView.annotations?.count != nil && mapView.annotations?.count > 1 ) {
+            mapView.removeAnnotation(mapView.annotations![0])
+        }
+        
+        // Add marker to the map
+        mapView.addAnnotation(mapPoint)
+        Connectivity.sharedInstance.getAddressFromCoordinate(tappedLocation.latitude, longitude: tappedLocation.longitude)
     }
 
     override func didReceiveMemoryWarning() {
