f15964bf4d3d4ec6bb1968d53bb38560b7bb1c76
sfink <sfink@devspark.com>
2016-08-09 18:49:34 -0300

    Commented and modified completionHandlers in MyBusService
    Modified handlers to be able to use new performRequest method in BaseNetworkingService

--- MyBus/MyBus/Services/MyBusService.swift
+++ MyBus/MyBus/Services/MyBusService.swift
@@ -35,6 +35,7 @@ public class MyBusService: NSObject, MyBusServiceDelegate {
 
     func searchRoutes(latitudeOrigin: Double, longitudeOrigin: Double, latitudeDestination: Double, longitudeDestination: Double, completionHandler: ([BusRouteResult]?, NSError?) -> ()) {
 
+        /*
         let availableBusLinesFromOriginDestinationURLString = "\(availableBusLinesFromOriginDestinationEndpointURL)lat0=\(latitudeOrigin)&lng0=\(longitudeOrigin)&lat1=\(latitudeDestination)&lng1=\(longitudeDestination)&tk=\(myBusAccessToken)"
 
         baseNetworkService.performRequest(availableBusLinesFromOriginDestinationURLString)
@@ -51,6 +52,22 @@ public class MyBusService: NSObject, MyBusServiceDelegate {
             busResults = BusRouteResult.parseResults(results, type: type)
             completionHandler(busResults, nil)
         }
+         */
+        let request = MyBusRouter.SearchRoutes(latOrigin: latitudeOrigin, lngOrigin: longitudeOrigin, latDest: latitudeDestination, lngDest: longitudeDestination, accessToken: MyBusRouter.MYBUS_ACCESS_TOKEN).URLRequest
+        
+        baseNetworkService.performRequest(request) { json, error in
+            guard json != nil else {
+                return completionHandler(nil, error)
+            }
+            
+            let type = json!["Type"].intValue
+            let results = json!["Results"]
+            let busResults: [BusRouteResult]
+            
+            busResults = BusRouteResult.parseResults(results, type: type)
+            completionHandler(busResults, nil)
+        }
+        
     }
 
     func searchRoads(roadType: MyBusRouteResultType, roadSearch: RoadSearch, completionHandler: (RoadResult?, NSError?) -> ()) {
@@ -67,8 +84,18 @@ public class MyBusService: NSObject, MyBusServiceDelegate {
         let beginStopSecondLine = roadSearch.beginStopSecondLine
         let endStopSecondLine = roadSearch.endStopSecondLine
 
-        let resultRoadURLString: String
+        //let resultRoadURLString: String
 
+        
+        var request:NSURLRequest
+        
+        if roadType == .Single {
+            request = MyBusRouter.SearchSingleRoad(idLine: idFirstLine, direction: firstDirection, beginStopLine: beginStopFirstLine, endStopLine: endStopFirstLine, accessToken: MyBusRouter.MYBUS_ACCESS_TOKEN).URLRequest
+        }else{
+            request = MyBusRouter.SearchCombinedRoad(idFirstLine: idFirstLine, idSecondLine: idSecondLine, firstDirection: firstDirection, secondDirection: secondDirection, beginStopFirstLine: beginStopFirstLine, endStopFirstLine: endStopFirstLine, beginStopSecondLine: beginStopSecondLine, endStopSecondLine: endStopSecondLine, accessToken: MyBusRouter.MYBUS_ACCESS_TOKEN).URLRequest
+        }
+        
+        /*
         switch roadType {
         case .Single:
             let singleResultRoadURLString = "\(singleResultRoadEndpointURL)idline=\(idFirstLine)&direction=\(firstDirection)&stop1=\(beginStopFirstLine)&stop2=\(endStopFirstLine)&tk=\(myBusAccessToken)"
@@ -81,6 +108,14 @@ public class MyBusService: NSObject, MyBusServiceDelegate {
         baseNetworkService.performRequest(resultRoadURLString)
         {
             response, error in
+            if let json = response {
+                completionHandler(RoadResult.parse(json), nil)
+            } else {
+                completionHandler(nil, error)
+            }
+        }*/
+        
+        baseNetworkService.performRequest(request) { response, error in
             if let json = response {
                 completionHandler(RoadResult.parse(json), nil)
             } else {
