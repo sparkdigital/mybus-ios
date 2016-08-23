45355a177cd711dee8adabd86f365ba91078910c
Julieta Gonzalez Poume <jpoume@devspark.com>
2016-08-23 15:47:18 -0300

    MYB 263

--- MyBus/MyBus/GenerateMessageAlert.swift
+++ MyBus/MyBus/GenerateMessageAlert.swift
@@ -17,4 +17,37 @@ class GenerateMessageAlert
         alert.addAction(action)
         viewController.presentViewController(alert, animated: true, completion: nil)
     }
+    
+    class func generateAlertToSetting(viewController: UIViewController){
+        
+        let alertController = UIAlertController (title: "Localización desactivada", message: "Para activar la localización: \n \n \n \n \n", preferredStyle: .Alert)
+        
+        let x = alertController.view.frame.origin.x
+        let y = alertController.view.frame.origin.y
+        alertController.view.frame=CGRectMake(x, y, alertController.view.frame.width, alertController.view.frame.height*0.25)
+        
+        let stepsView = loadFromNibNamed("StepActivateLocate")
+        stepsView!.frame=CGRectMake(x+25, y+65, (stepsView?.frame.width)!, (stepsView?.frame.height)!)
+        alertController.view.addSubview(stepsView!)
+        
+        let settingsAction = UIAlertAction(title: "Configuración", style: .Default) { (_) -> Void in
+            let settingsUrl = NSURL(string: UIApplicationOpenSettingsURLString)
+            if let url = settingsUrl {
+                UIApplication.sharedApplication().openURL(url)
+            }
+        }
+        alertController.addAction(settingsAction)
+        
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .Default, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        viewController.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    class func loadFromNibNamed(nibNamed: String, bundle: NSBundle? = nil) -> UIView? {
+        return UINib(
+            nibName: nibNamed,
+            bundle: bundle
+            ).instantiateWithOwner(nil, options: nil)[0] as? UIView
+    }
 }
