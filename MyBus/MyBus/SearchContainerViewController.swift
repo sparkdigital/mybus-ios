4fa42bae4d786f1415acb426c7f34421e30c6007
sfink <sfink@devspark.com>
2016-09-26 18:06:06 -0300

    -Added classes for handling a search container between search options and search suggestions

---
+++ MyBus/MyBus/SearchContainerViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SearchContainerViewController.swift
+//  MyBus
+//
+//  Created by Sebastian Fink on 9/26/16.
+//  Copyright © 2016 Spark Digital. All rights reserved.
+//
+
+import UIKit
+
+class SearchContainerViewController: UIViewController {
+
+    @IBOutlet weak var addressLocationSearchBar: UISearchBar!
+    
+    @IBOutlet weak var searchContainerView: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
