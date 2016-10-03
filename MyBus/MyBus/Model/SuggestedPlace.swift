80055c37656fe830c4598c03eaebf88e458659b9
Lisandro Falconi <lisandrofalconi@gmail.com>
2016-10-03 15:33:21 -0300

    Added SuggestedPlace model class

---
+++ MyBus/MyBus/Model/SuggestedPlace.swift
@@ -0,0 +1,26 @@
+//
+//  SuggestedPlace.swift
+//  MyBus
+//
+//  Created by Lisandro Falconi on 10/3/16.
+//  Copyright © 2016 Spark Digital. All rights reserved.
+//
+
+import Foundation
+
+class SuggestedPlace {
+    var name: String = ""
+    var description: String?
+    var address: String?
+    var photoUrl: String?
+    var location: (latitude: Double, longitude: Double)
+
+    init(object: NSDictionary){
+        self.name = object.objectForKey("name") as! String
+        self.description = object.objectForKey("description") as? String
+        self.address = object.objectForKey("address") as? String
+        self.photoUrl = object.objectForKey("photoUrl") as? String
+        self.location = (object.objectForKey("lat") as! Double, object.objectForKey("lng") as! Double)
+    }
+
+}
