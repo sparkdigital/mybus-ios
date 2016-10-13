95266a4b2f95bbb8832b56bcf1ba4ee8995be966
Lisandro Falconi <lisandrofalconi@gmail.com>
2016-10-13 15:24:14 -0300

    Added icons to be used in buses table view cells

---
+++ MyBus/MyBus/Cells/SimpleResultTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  SimpleResultTableViewCell.swift
+//  MyBus
+//
+//  Created by Lisandro Falconi on 10/11/16.
+//  Copyright © 2016 Spark Digital. All rights reserved.
+//
+
+import Foundation
