fb37bd41d5d7ba5e9b52d12ffb3f6299be30889f
Lisandro Falconi <lisandrofalconi@gmail.com>
2016-11-02 16:54:55 -0300

    Prevented search table view from be overlapped by keyboard setting a footer view with height of keyboard

--- MyBus/MyBus/SearchViewController.swift
+++ MyBus/MyBus/SearchViewController.swift
@@ -29,7 +29,14 @@ protocol MainViewDelegate: class {
 
 class SearchViewController: UIViewController, UITableViewDelegate
 {
-
+    
+    //Variable with a hardcoded height (usually is around this value)
+    let kSearchBarNavBarHeight:CGFloat = 140.0
+    let kMinimumKeyboardHeight:CGFloat = 216.0 + 140.0
+    
+    //Control variable to see if we're using the search textfield or not
+    var isSearching:Bool = false
+    
     @IBOutlet weak var searchTableView: UITableView!
 
     var mainViewDelegate: MainViewDelegate?
@@ -52,7 +59,20 @@ class SearchViewController: UIViewController, UITableViewDelegate
         let view = UINib(nibName:"HeaderTableView", bundle: nil).instantiateWithOwner(nil, options: nil)[0] as! UIView
         view.addGestureRecognizer(tap)
         self.searchTableView.tableHeaderView = view
-        self.searchTableView.tableFooterView = UIView(frame: CGRectMake(0.0,0.0,self.view.frame.width,140.0))
+
+        
+        //Custom code
+        
+        // Listen for keyboard changes (if it's showing or hiding)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(self.keyboardWasShown(_:)), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(self.keyboardWasHidden(_:)), name: UIKeyboardWillHideNotification, object: nil)
+        
+        //You could initialize nonetheless the table footer with a custom height
+        self.setupTableViewFooter(kMinimumKeyboardHeight)
+    }
+
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
     }
 
     func tappedCurrentLocation(){
@@ -77,6 +97,40 @@ class SearchViewController: UIViewController, UITableViewDelegate
             self.mainViewDelegate?.loadPositionFromFavsRecents(selectedFavourite)
         default:
             break
+        }
+    }
+    
+    // MARK: Keyboard was shown or hidden
+    func keyboardWasShown(sender:NSNotification){
+        self.isSearching = true
+        
+        guard let info: NSDictionary = sender.userInfo else {
+            NSLog("SearchCountry - No user info found in notification")
+            return
+        }
+        
+        guard let value: NSValue = info.valueForKey(UIKeyboardFrameBeginUserInfoKey) as? NSValue else {
+            NSLog("SearchCountry - No frame found for keyboard in userInfo")
+            return
+        }
+        
+        //Get the current keyboard size (I guess it varies across devices)
+        let keyboardSize: CGSize = value.CGRectValue().size
+        self.setupTableViewFooter(keyboardSize.height + kSearchBarNavBarHeight)
+        
+    }
+    
+    // Setup an empty footer
+    func keyboardWasHidden(sender:NSNotification){
+        self.isSearching = false
+        self.setupTableViewFooter(0.0)
+    }
+    
+    private func setupTableViewFooter(height:CGFloat){
+        if height > 0.0 {
+            self.searchTableView.tableFooterView = UIView(frame: CGRectMake(0.0,0.0,self.view.frame.width,height))
+        }else{
+            self.searchTableView.tableFooterView = UIView(frame:CGRectZero)
         }
     }
 }
