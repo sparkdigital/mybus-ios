2c27ffd3b447496ccb801405003a574f97bf2737
Marcos Vivar <mvivar@devspark.com>
2016-11-30 17:30:09 -0300

    Updated tests for DataSources.

--- MyBus/MyBusTests/DataSources/BusesInformationDataSourceTest.swift
+++ MyBus/MyBusTests/DataSources/BusesInformationDataSourceTest.swift
@@ -8,28 +8,22 @@
 
 import XCTest
 
-class BusesInformationDataSourceTest: XCTestCase {
+class BusesInformationDataSourceTest: XCTestCase
+{
+    let busesInformationDataSource: BusesInformationDataSource = BusesInformationDataSource()
     
-    override func setUp() {
+    override func setUp()
+    {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown()
+    {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testResultExistence()
+    {
+        XCTAssertNotNil(busesInformationDataSource.busInformation)
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
