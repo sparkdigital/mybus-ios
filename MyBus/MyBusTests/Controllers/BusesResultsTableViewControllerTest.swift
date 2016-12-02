2d297480d0d7dfc04404465bde2565df5f1e110e
Marcos Vivar <mvivar@devspark.com>
2016-12-02 17:50:59 -0300

    Updated test for controllers.

--- MyBus/MyBusTests/Controllers/BusesResultsTableViewControllerTest.swift
+++ MyBus/MyBusTests/Controllers/BusesResultsTableViewControllerTest.swift
@@ -8,28 +8,29 @@
 
 import XCTest
 
-class BusesResultsTableViewControllerTest: XCTestCase {
+class BusesResultsTableViewControllerTest: XCTestCase
+{
+    let busesResultsController: BusesResultsTableViewController = BusesResultsTableViewController()
     
-    override func setUp() {
+    override func setUp()
+    {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown()
+    {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testResultExistence()
+    {
+        XCTAssertNotNil(busesResultsController)
+        XCTAssertNotNil(busesResultsController.buses)
+        XCTAssertNotNil(busesResultsController.simpleCellIdentifier)
+        XCTAssertNotNil(busesResultsController.combinedCellIdentifier)
+        XCTAssertNotNil(busesResultsController.tableView)
+
+        XCTAssertNil(busesResultsController.busSearchResult)
+        XCTAssertNil(busesResultsController.mainViewDelegate)
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
