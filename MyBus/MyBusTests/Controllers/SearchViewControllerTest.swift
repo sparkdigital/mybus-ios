6764d9bdd2b123721f4527c2cdd539f06a9b91bf
Marcos Vivar <mvivar@devspark.com>
2016-12-02 16:06:10 -0300

    Added test for controllers classes.

---
+++ MyBus/MyBusTests/Controllers/SearchViewControllerTest.swift
@@ -0,0 +1,46 @@
+//
+//  SearchViewControllerTest.swift
+//  MyBus
+//
+//  Created by Marcos Jesus Vivar on 12/1/16.
+//  Copyright © 2016 Spark Digital. All rights reserved.
+//
+
+import XCTest
+import RealmSwift
+import MyBus
+
+class SearchViewControllerTest: XCTestCase {
+    
+    let searchController: SearchViewController = SearchViewController()
+    
+    override func setUp()
+    {
+        super.setUp()
+    }
+    
+    override func tearDown()
+    {
+        super.tearDown()
+    }
+    
+    func testResultExistence()
+    {
+        XCTAssertNotNil(searchController)
+        XCTAssertNotNil(searchController.busResults)
+        XCTAssertNotNil(searchController.bestMatches)
+        XCTAssertNotNil(searchController.progressNotification)
+        
+        XCTAssertNotNil(searchController.kSearchBarNavBarHeight)
+        XCTAssertEqual(searchController.kSearchBarNavBarHeight, 140.0)
+        XCTAssertNotNil(searchController.kMinimumKeyboardHeight)
+        XCTAssertEqual(searchController.kMinimumKeyboardHeight, 356.0)
+        
+        XCTAssert(!searchController.isSearching)
+        
+        XCTAssertNil(searchController.searchTableView)
+        XCTAssertNil(searchController.mainViewDelegate)
+        XCTAssertNil(searchController.favourites)
+        XCTAssertNil(searchController.streetSuggestionsDataSource)
+    }
+}
---
+++ MyBus/MyBusTests/Controllers/SuggestionSearchViewControllerTest.swift
@@ -0,0 +1,71 @@
+//
+//  SuggestionSearchViewControllerTest.swift
+//  MyBus
+//
+//  Created by Marcos Jesus Vivar on 12/1/16.
+//  Copyright © 2016 Spark Digital. All rights reserved.
+//
+
+import XCTest
+
+class SuggestionSearchViewControllerTest: XCTestCase
+{
+    let suggestionSearchController: SuggestionSearchViewController = SuggestionSearchViewController()
+    
+    override func setUp()
+    {
+        super.setUp()
+    }
+    
+    override func tearDown()
+    {
+        super.tearDown()
+    }
+    
+    func testResultExistence()
+    {
+        XCTAssertNotNil(suggestionSearchController)
+        XCTAssertNotNil(suggestionSearchController.bestMatches)
+        
+        XCTAssertNil(suggestionSearchController.searchSuggestionTableView)
+        XCTAssertNil(suggestionSearchController.suggestionsDataSource)
+        XCTAssertNil(suggestionSearchController.searchBarController)
+        XCTAssertNil(suggestionSearchController.mainViewDelegate)
+        XCTAssertNil(suggestionSearchController.searchBar)
+    }
+    
+    func testResultContents()
+    {
+        let avenues:[SuggestionProtocol] = suggestionSearchController.applyFilter("Av ")
+        XCTAssert(avenues.count > 0)
+        for case let item:SuggestionProtocol in avenues
+        {
+            XCTAssertNotNil(item.name)
+            XCTAssertNotNil(item.getPoint())
+            
+            let point:RoutePoint = item.getPoint()
+            XCTAssertNotNil(point.stopId)
+            XCTAssertNotNil(point.latitude)
+            XCTAssertNotNil(point.longitude)
+            XCTAssertNotNil(point.address)
+            XCTAssertNotNil(point.isWaypoint)
+            XCTAssertNotNil(point.name)
+        }
+        
+        let diagonals:[SuggestionProtocol] = suggestionSearchController.applyFilter("Diag ")
+        XCTAssert(diagonals.count > 0)
+        for case let item:SuggestionProtocol in diagonals
+        {
+            XCTAssertNotNil(item.name)
+            XCTAssertNotNil(item.getPoint())
+            
+            let point:RoutePoint = item.getPoint()
+            XCTAssertNotNil(point.stopId)
+            XCTAssertNotNil(point.latitude)
+            XCTAssertNotNil(point.longitude)
+            XCTAssertNotNil(point.address)
+            XCTAssertNotNil(point.isWaypoint)
+            XCTAssertNotNil(point.name)
+        }
+    }
+}
