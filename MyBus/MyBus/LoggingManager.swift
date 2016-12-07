2a4015ec1009fb03d5ea3c26efd09f5bf5a069e7
sfink <sfink@devspark.com>
2016-12-07 19:14:32 -0300

    Added LoggingManager class to the project with required methods to work with Flurry and Crashlytics at the same time

---
+++ MyBus/MyBus/LoggingManager.swift
@@ -0,0 +1,77 @@
+//
+//  LoggingManager.swift
+//  MyBus
+//
+//  Created by Sebastian Fink on 12/7/16.
+//  Copyright © 2016 Spark Digital. All rights reserved.
+//
+
+import Foundation
+import Crashlytics
+import Flurry_iOS_SDK
+
+private let _sharedInstance = LoggingManager()
+
+public class LoggingManager: NSObject {
+    
+    private static let flurryApiKey:String = "75KB8D2BW66PV5M9QSGW"
+    private static var logTimestamp:NSTimeInterval {
+        return NSDate().timeIntervalSince1970 * 1000
+    }
+    
+    var crashlyticsClassInstance:AnyObject {
+        return Crashlytics.self
+    }
+    
+    // MARK: - Singleton Instantiation
+    public class var sharedInstance: LoggingManager {
+        return _sharedInstance
+    }
+    
+    private override init() {
+        super.init()
+    }
+    
+    func setup(){
+        setupCrashlytics()
+        setupFlurry()
+    }
+    
+    private func setupCrashlytics(){
+        //No custom setup at the moment
+    }
+    
+    private func setupFlurry(){
+        Flurry.setLogLevel(FlurryLogLevelDebug)
+        Flurry.setCrashReportingEnabled(true)
+        Flurry.startSession(LoggingManager.flurryApiKey)
+    }
+    
+    private func logCrashlytics(event:String){
+        Answers.logCustomEventWithName(event, customAttributes: ["evt_timestamp" : LoggingManager.logTimestamp])
+    }
+    
+    private func logFlurry(event:String){
+        Flurry.logEvent(event)
+    }
+    
+    // MARK: Public interface
+    func startAppLogging(){
+        logCrashlytics("App Started")
+        logFlurry("App Started")
+    }
+    
+    func logSection(section:String){
+        let legend:String = "Switched to \(section)"
+        logCrashlytics(legend)
+        logFlurry(legend)
+    }
+    
+    func logError(sender:String, error:NSError?){
+        let errorDescription = error?.description ?? "No description available"
+        let legend:String = "\(sender) - Error: \(errorDescription)"
+        logCrashlytics(legend)
+        logFlurry(legend)        
+    }
+    
+}
