d8ebfd132ed78e35a5c5b5a153b81ea59d4ac173
Sebastian Fink <sfink@sparkdigital.com>
2016-12-22 19:04:35 -0300

    Improved logging manager, - added setup with firebase and libraries import - added methods to log events, sections and errors and implemented them for firebase as well - added enumerations of loggable sections and loggable events to have them as parameters

--- MyBus/MyBus/LoggingManager.swift
+++ MyBus/MyBus/LoggingManager.swift
@@ -9,8 +9,40 @@
 import Foundation
 import Crashlytics
 import Flurry_iOS_SDK
+import Firebase
+import FirebaseMessaging
+import FirebaseAnalytics
 
 private let _sharedInstance = LoggingManager()
+
+enum LoggableAppSection:String {
+    case MAP = "Search and Map Visualization"
+    case FAVOURITES = "Favourites"
+    case RECHARGE = "Recharge Points"
+    case ROUTES = "Bus Routes"
+    case FARES = "Bus Fares"
+    case TERMS = "Terms and Conditions"
+    case ABOUT = "About Us"
+}
+
+enum LoggableAppEvent:String {
+    case FAVORITE_NEW_PLUS = "New Favorite from form"
+    case FAVORITE_NEW_MARKER = "New Favorite from map annotation"
+    case FAVORITE_DEL_LIST = "Removed Favorite from Wishlist"
+    case FAVORITE_DEL_MARKER = "Removed Favorite from marker"
+    case FAVORITE_EDIT_LIST = "Favorite content was edited"
+    case MARKER_DRAGGED = "Endpoint dragged in map"
+    case INVERT_TAPPED = "Invert endpoints"
+    case ROUTE_SELECTED = "Bus route selected"
+    case ROUTE_GOING_TAPPED = "Bus route selector going"
+    case ROUTE_RETURN_TAPPED = "Bus route selector return"
+    case ENDPOINT_FROM_LONGPRESS = "Endpoint from map long press"
+    case ENDPOINT_FROM_SUGGESTION = "Endpoint from suggestion"
+    case ENDPOINT_FROM_RECENTS = "Endpoint from recent location"
+    case ENDPOINT_FROM_FAVORITES = "Endpoint from favorite location"
+    case ENDPOINT_GPS_MAP = "Endpont from current location - Map"
+    case ENDPOINT_GPS_SEARCH = "Endpoint from current location - Search"
+}
 
 public class LoggingManager: NSObject {
 
@@ -35,6 +67,12 @@ public class LoggingManager: NSObject {
     func setup(){
         setupCrashlytics()
         setupFlurry()
+        setupFirebase()
+    }
+    
+    private func setupFirebase(){
+        //Initialize Firebase
+        FIRApp.configure()
     }
 
     private func setupCrashlytics(){
@@ -54,17 +92,37 @@ public class LoggingManager: NSObject {
     private func logFlurry(event: String){
         Flurry.logEvent(event)
     }
-
+    
+    private func logFirebaseEvent(event:LoggableAppEvent){
+        FIRAnalytics.logEventWithName(event.rawValue, parameters: nil)
+    }
+    
+    private func logFirebaseSection(section:LoggableAppSection){
+        FIRAnalytics.logEventWithName(section.rawValue, parameters: nil)
+    }
+    
+    private func logFirebaseError(error:String){
+        FIRAnalytics.logEventWithName("Error", parameters: [kFIRParameterValue:error])
+    }
+    
     // MARK: Public interface
     func startAppLogging(){
-        logCrashlytics("App Started")
-        logFlurry("App Started")
+        logCrashlytics("AppStarted")
+        logFlurry("AppStarted")
     }
 
-    func logSection(section: String){
-        let legend: String = "Switched to \(section)"
+    func logSection(section: LoggableAppSection){
+        let legend: String = "Entered to \(section.rawValue)"
         logCrashlytics(legend)
         logFlurry(legend)
+        logFirebaseSection(section)
+    }
+    
+    func logEvent(event: LoggableAppEvent){
+        let legend:String = "Event = \(event.rawValue)"
+        logFlurry(legend)
+        logCrashlytics(legend)
+        logFirebaseEvent(event)
     }
 
     func logError(sender: String, error: NSError?){
@@ -72,6 +130,7 @@ public class LoggingManager: NSObject {
         let legend: String = "\(sender) - Error: \(errorDescription)"
         logCrashlytics(legend)
         logFlurry(legend)
+        logFirebaseError(errorDescription)
     }
 
 }
